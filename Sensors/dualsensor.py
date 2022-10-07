dca058ffec86b61d971266af0f66e96fd3a2a2dc
Will Li <45328760+will-wz-li@users.noreply.github.com>
2022-10-07 18:42:08 +0800

    added LED, parking spot check function

--- Sensors/dualsensor.py
+++ Sensors/dualsensor.py
@@ -9,12 +9,12 @@ import time
 from datetime import datetime
 import sys
 
-################ Ultrasonic
 #set up 
-GPIO.setmode(GPIO.BOARD)
+################ Ultrasonic
+GPIO.setmode(GPIO.BCM)
 
-PIN_TRIGGER = 7
-PIN_ECHO = 11
+PIN_TRIGGER = 4
+PIN_ECHO = 17
 
 GPIO.setup(PIN_TRIGGER, GPIO.OUT)
 GPIO.setup(PIN_ECHO, GPIO.IN)
@@ -30,20 +30,23 @@ current_time = datetime.now()
 current_time = current_time.strftime("%Y-%d-%M-%H-%M-%S")
 identifier = ''.join(sys.argv[1:2])
 
-file_object = open(f'Data_{identifier}.txt', 'a')
+#file_object = open(f'Data_{identifier}.txt', 'a')
+
+############### LED
+PIN_LED = 27
+GPIO.setup(PIN_LED, GPIO.OUT)
+
     
 def magneto():
 
     strength = magSensor.readMagnitude()   # Reads the magnetic-field strength in microTesla (uT)
-    myString = str(strength) + ' uT'       # create a string with the field-strength and the unit
-    print(myString)                        # Print the field strength
-    
-    return strength
+
     
     if strength > threshold:               # Check if the magnetic field is strong
         print('Strong Magnet!')
 
     sleep_ms(1000)
+    return strength
     
 def distance():    
     # init
@@ -68,23 +71,62 @@ def distance():
     distance = round(pulse_duration * 17150, 2)
     return distance
 
+def init_sensor(mag_baseline):
+    # find the baseline readings for magnetometer
+    # when the park space is unoccupied
+    mag_baseline = magneto()
+    
+    
+
+def check_parking_spot():
+    # obtain 3 measurements with 10s interval, compute average
+    # conditions of an occupied space:
+    # - distance < 1m
+    # - magnetic field increase > 20% of baseline
+    occupied = 0 
+    # 1
+    dist = distance()
+    mag = magneto()
+    time.sleep(5)
+    # 2
+    dist += distance()
+    mag += magneto()
+    time.sleep(5)
+    # 3
+    dist += distance()
+    mag += magneto()
+    # average reading
+    dist = dist/3
+    msg = ("Distance:" + str(dist) + "cm")
+    print(msg)
+    
+    mag = mag/3
+    myString = str(mag) + ' uT'       # create a string with the field-strength and the unit
+    print(myString)                        # Print the field strength
+    
+    if (dist < 100): #100cm = 1m
+        print(mag_baseline)
+        if (mag/mag_baseline > 1.2):
+           occupied = 1
+    
+        
+    return occupied
 
     
 if __name__ == '__main__':
+    
     try:
+        mag_baseline = magneto()
+        
         while True:
-            dist = distance()
-            msg = ("Distance:" + str(dist) + "cm")
-            print(msg)
-            time.sleep(1)
-            
-            mag = magneto()
+            occupied = check_parking_spot()
+            GPIO.output(PIN_LED, occupied)
             
             date = datetime.now()
-            file_object.write(str(dist) + " " + str(mag) + " "+
-                            str(date)+ "\n")
+            #file_object.write(str(dist) + " " + str(mag) + " "+
+            #                str(date)+ "\n")
     except KeyboardInterrupt:
         print("Stopped")
         GPIO.cleanup()
-        file_object.close()
+        #file_object.close()
     
