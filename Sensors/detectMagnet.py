da5a5bfb1e9875849bbdf01e539861d9d7a770b4
Will Li <45328760+will-wz-li@users.noreply.github.com>
2022-10-11 12:05:14 +0800

    add 2 more Mag sensors

--- Sensors/detectMagnet.py
+++ Sensors/detectMagnet.py
@@ -3,18 +3,24 @@
 from PiicoDev_QMC6310 import PiicoDev_QMC6310
 from PiicoDev_Unified import sleep_ms
 
-magSensor = PiicoDev_QMC6310(range=3000) # initialise the magnetometer
+magSensor1 = PiicoDev_QMC6310(bus=1, range=3000) # initialise the magnetometer
 # magSensor.calibrate()
+magSensor2 = PiicoDev_QMC6310(bus=4, range=3000) # initialise the magnetometer
+magSensor3 = PiicoDev_QMC6310(bus=3, range=3000) # initialise the magnetometer
 
 threshold =  300# microTesla or 'uT'.
 
-while True:
-
+def readStrength(magSensor, number):
+    
     strength = magSensor.readMagnitude()   # Reads the magnetic-field strength in microTesla (uT)
-    myString = str(strength) + ' uT'       # create a string with the field-strength and the unit
+    myString = number + ' : ' + str(strength) + ' uT'       # create a string with the field-strength and the unit
     print(myString)                        # Print the field strength
     
     if strength > threshold:               # Check if the magnetic field is strong
         print('Strong Magnet!')
-
+    
+while True:
+    readStrength(magSensor1, '1')
+    readStrength(magSensor2, '2')
+    readStrength(magSensor3, '3')
     sleep_ms(1000)
